d6bcb6004d33d1f2b617b5f66d80f92cf214e63e
shanbora <chanbora@logilink.fr>
2024-11-20 16:52:58 +0100

    init layout user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/espace-membre', name: 'user_')]
+#[Route('/espace-pro', name: 'user_')]
 class UserController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
